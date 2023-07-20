581f65cc3483a4897a121cc6fb7143c3e00c559e
biwsantang <biw.santang@icloud.com>
2023-07-20 22:20:58 +0700

    install.sh for macOS

--- install.sh
+++ install.sh
@@ -1,29 +1,47 @@
 #!/bin/bash
 
-# Function to install Homebrew
-install_brew() {
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-}
+# Variables
+dir=$HOME/.dotfiles                    # dotfiles directory
+packages="bash vim zsh tmux"           # list of packages to stow
+source_line="for file in ~/.config/*.zsh; do source \$file; done"
 
-# Function to install packages from Brewfile
-install_packages() {
-  brew bundle install
-}
-
-# Check if macOS
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  # Check if Homebrew is installed
-  if ! command -v brew &> /dev/null; then
-    echo "Homebrew not found. Installing..."
-    install_brew
-  fi
-  # Assume Brewfile is in the same directory
-  if [ -f "./Brewfile" ]; then
-    echo "Installing packages from Brewfile..."
-    install_packages
-  else
-    echo "Brewfile not found in current directory."
-  fi
-else
-  echo "This script is designed to run on macOS only."
+# Install Homebrew if it's not already installed
+if ! command -v brew &> /dev/null
+then
+    echo "Homebrew could not be found, installing now..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
+
+# Install packages from Brewfile
+if [[ -f "$dir/Brewfile" ]]; then
+    echo "Installing packages from Brewfile..."
+    brew bundle --file="$dir/Brewfile"
+else
+    echo "Brewfile not found. Skipping..."
+fi
+
+# Install stow
+if brew ls --versions stow > /dev/null; then
+    echo "Stow is already installed, skipping installation..."
+else
+    echo "Stow is not installed, installing now..."
+    brew install stow
+fi
+
+# Change to the dotfiles directory
+echo "Changing to the $dir directory"
+cd $dir
+
+# Stow packages
+for package in $packages; do
+    stow $package
+done
+
+# Source all *.zsh files in ~/.config, if not already present
+if ! grep -Fxq "$source_line" ~/.zshrc
+then
+    echo "$source_line" >> ~/.zshrc
+else
+    echo "Source line already exists in .zshrc, skipping..."
+fi
+
