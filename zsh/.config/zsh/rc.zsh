f6dfc08b13ce141b638f3cd1431e38922e725475
Biw Santang <biw.santang@icloud.com>
2024-05-08 22:46:22 +0700

    add forgit zshrc

--- zsh/.config/zsh/rc.zsh
+++ zsh/.config/zsh/rc.zsh
@@ -6,3 +6,5 @@ _comp_options+=(globdots)
 
 autoload edit-command-line; zle -N edit-command-line
 bindkey '^e' edit-command-line
+
+[ -f $HOMEBREW_PREFIX/share/forgit/forgit.plugin.zsh ] && source $HOMEBREW_PREFIX/share/forgit/forgit.plugin.zsh
